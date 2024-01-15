506694e07575e97aaeb73a3461b3e5a8880d5e3f
SonMyeongJin <smj2802@naver.com>
2024-01-16 04:04:14 +0900

    first setting

---
+++ src/ChatServer.java
@@ -0,0 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ChatServer {
+
+    ServerSocket serverSocket;
+    ExecutorService threadPool = Executors.newFixedThreadPool(100);
+    Map<String, SocketClient> chatRoom = Collections.synchronizedMap(new HashMap<>());
+
+
+    public static void maid(String[] arg){
+
+
+    }
+}
