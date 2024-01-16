21318f96f3e89e5f4a33050cc2101495f6b2b549
SonMyeongJin <smj2802@naver.com>
2024-01-17 00:14:37 +0900

    Member add / remove

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,3 +1,5 @@
+import javax.crypto.SealedObject;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
@@ -6,12 +8,42 @@ import java.util.concurrent.Executors;
 
 public class ChatServer {
 
+    //필드
     ServerSocket serverSocket;
     ExecutorService threadPool = Executors.newFixedThreadPool(100);
     Map<String, SocketClient> chatRoom = Collections.synchronizedMap(new HashMap<>());
+
+    //메소드
+    public void start() throws IOException {
+        serverSocket = new ServerSocket(50001);
+        System.out.println(" [server] start");
+
+        Thread thread = new Thread(()-> {
+            try {
+                Socket socket = serverSocket.accept();
+                SocketClient sc = new SocketClient(this, socket);
+            } catch (Exception e) {
+            }
+        });
+        thread.start();
+    }
+
+    //채팅방에 참가자 추가
+    public void addSocketClient(SocketClient socketClient) {
+        String key = socketClient.chatName + "@" + socketClient.clientIp;
+        chatRoom.put(key, socketClient);
+        System.out.println("Enter: " + key);
+        System.out.println("Number of member: " + chatRoom.size() + "\n");
+    }
+
+    public void removeSocketClient(SocketClient socketClient) {
+        String key = socketClient.chatName + "@" + socketClient.clientIp;
+        chatRoom.remove(key);
+        System.out.println("Out: " + key);
+        System.out.println("Number of member: " + chatRoom.size() + "\n");
+    }
+
     public static void maid(String[] arg){
-
-
 
     }
 }
--- src/SocketClient.java
+++ src/SocketClient.java
@@ -1,2 +1,15 @@
+import java.net.Socket;
+
 public class SocketClient {
+    //필드
+    ChatServer chatServer;
+    Socket socket;
+    String clientIp;
+    String chatName;
+
+    //생성자
+    public SocketClient(ChatServer chatServer, Socket socket){
+        this.chatServer = chatServer;
+        this.socket = socket;
+    }
 }
